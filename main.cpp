0ca3de1edcab3798a0571f7a65698c4ff29d6a2b
Robbie Langer <robbie@sohio.net>
2021-12-01 14:10:44 -0500

    and on the left; constexprize constants

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "FEHLCD.h"
 #include "FEHUtility.h"
 
+constexpr int XMIN = -2;
+constexpr int XMAX = 284;
+constexpr int YMIN = 0;
+constexpr int YMAX = 226;
+
 /*
  * Entry point to the application
  */
@@ -9,7 +14,7 @@ int main() {
     LCD.SetBackgroundColor(BLACK);
     LCD.Clear();
 
-    int x = -1, y = 0, dx = 1, dy = 1;
+    int x = XMIN, y = YMIN, dx = 1, dy = 1;
 
     while (1) {
 	LCD.Clear();
@@ -17,19 +22,19 @@ int main() {
         LCD.Update();
         //Sleep(20);
 	x += dx, y += dy;
-	if (x >= 284) {
+	if (x >= XMAX) {
 		dx *= -1;
-		x = 284;
-	} else if (x <= -1) {
+		x = XMAX;
+	} else if (x <= XMIN) {
 		dx *= -1;
-		x = -1;
+		x = XMIN;
 	}
-	if (y >= 226) {
+	if (y >= YMAX) {
 		dy *= -1;
-		y = 226;
-	} else if (y <= 0) {
+		y = YMAX;
+	} else if (y <= YMIN) {
 		dy *= -1;
-		y = 0;
+		y = YMIN;
 	}
         // Never end
     }
