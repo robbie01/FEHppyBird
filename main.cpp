f6456d50f23943340c4b7353dd26d6b889193381
Robbie Langer <robbie@sohio.net>
2021-12-01 11:10:36 -0500

    replace main w bouncy ball demo

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "FEHLCD.h"
+#include "FEHUtility.h"
 
 /*
  * Entry point to the application
@@ -8,10 +9,28 @@ int main() {
     LCD.SetBackgroundColor(BLACK);
     LCD.Clear();
 
-    LCD.WriteLine("Hello World!");
-    
+    int x = 20, y = 20, dx = 2, dy = 2;
+
     while (1) {
+	LCD.Clear();
+        LCD.DrawCircle(x, y, 20);
         LCD.Update();
+        //Sleep(20);
+	x += dx, y += dy;
+	if (x >= 299) {
+		dx *= -1;
+		x = 299;
+	} else if (x <= 20) {
+		dx *= -1;
+		x = 20;
+	}
+	if (y >= 219) {
+		dy *= -1;
+		y = 219;
+	} else if (y <= 20) {
+		dy *= -1;
+		y = 20;
+	}
         // Never end
     }
     return 0;
