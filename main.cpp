dd4f1d04e028c1c930938f312be43d5224c64bdb
lukew3 <lukew25073@gmail.com>
2021-12-02 09:02:50 -0500

    -

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ constexpr int PIPEWIDTH = 20;
 constexpr int PIPEVELOCITY = -2;
 constexpr int PIPEXMIN = 0;
 constexpr int PIPEXMAX = SCREENWIDTH - PIPEWIDTH;
-constexpr int PIPEGAPMIN = 0; constexpr int PIPEGAPMAX = SCREENHEIGHT - PIPEGAPSIZE;
+constexpr int PIPEGAPMIN = 0;
+constexpr int PIPEGAPMAX = SCREENHEIGHT - PIPEGAPSIZE;
 
 constexpr int BIRDHEIGHT = 20;
 constexpr int BIRDWIDTH = 20;
